5d4b1e6210597a01bc3ca23e3a334e6ef4cab54e
Masahiro111 <33553834+Masahiro111@users.noreply.github.com>
2021-06-24 02:35:31 +0900

    fix part 2

--- resources/views/product/index.blade.php
+++ resources/views/product/index.blade.php
@@ -9,7 +9,7 @@ Top
 @include('layouts.products.navi')
 
 <section class="w-full mx-auto bg-nordic-gray-light flex pt-12 md:pt-0 md:items-center bg-cover bg-right"
-    style="max-width:1600px; height: 32rem; background-image: url('https://images.unsplash.com/photo-1422190441165-ec2956dc9ecc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1600&q=80');">
+    style="max-width:1600px; height: 32rem; background-image: url('{{ asset('images/top.bmp') }}');">
     <div class="container mx-auto">
         <div class="flex flex-col w-full lg:w-1/2 justify-center items-start  px-6 tracking-wide">
             <h1 class="text-black text-2xl my-4">Stripy Zig Zag Jigsaw Pillow and Duvet Set</h1>
@@ -81,4 +81,4 @@ Top
 
 @include('layouts.products.footer')
 
-@endsction
+@endsection
