b6093f463ed63b2987c828ced7dba2a9774d4fda
Masahiro111 <33553834+Masahiro111@users.noreply.github.com>
2021-06-29 01:17:55 +0900

    finished part 3

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,12 +2,39 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class PostController extends Controller
 {
     public function index()
     {
         return view('qiita.drafts.new');
+    }
+
+    public function postArticle(Request $request)
+    {
+        // validation
+        $request->validate([
+            'title' => 'required|unique:posts|max:255',
+            'tags' => 'required|string',
+            'article' => 'required|string',
+        ]);
+
+        $tags = explode(' ', $request->tags);
+        $tag1 = $tags[0];
+        $tag2 = (isset($tags[1])) ? $tags[1] : null;
+        $tag3 = (isset($tags[2])) ? $tags[2] : null;
+
+        $article = Post::create([
+            'user_id' => Auth::user()->id,
+            'title' => $request->title,
+            'tag1' => $tag1,
+            'tag2' => $tag2,
+            'tag3' => $tag3,
+            'body' => $request->article,
+        ]);
+        return redirect('/qiita');
     }
 }
