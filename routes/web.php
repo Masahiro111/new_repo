45506efdfc6d1586064ab395ff977a4bacaa1c59
Masahiro111 <33553834+Masahiro111@users.noreply.github.com>
2021-07-10 16:55:38 +0900

    Revert "finished part 2"
    This reverts commit ac31a4e61eecbff5f56670812039ca29e67b9391.

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,11 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-
-Route::get('/', [ProductController::class, 'index'])->name('product.index');
+Route::get('/', function () {
+    return view('welcome');
+});
 
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
-require __DIR__ . '/auth.php';
+require __DIR__.'/auth.php';
