6b2106e1fcdb7e34ed9a7f8b42666326634ab7cc
Masahiro111 <33553834+Masahiro111@users.noreply.github.com>
2021-07-14 23:57:01 +0900

    4-3

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [ArticleController::class, 'index']);
 
-Route::resource('/articles', ArticleController::class);
+Route::resource('/articles', 'ArticleController')
+    ->except(['index'])
+    ->middleware('auth');
 
 Route::resource('/test', TestController::class);
 
